075d6393f3db69078c862c6a4ecd132e72f0dd17
anshuleppa123-collab <anshuleppa123@gmail.com>
2026-06-12 06:08:41 +0000

    First Streamlit App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Welcome to Streamlit")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.success(f"Hello {name}")
+
+age = st.slider("Select your age", 1, 100)
+
+st.write("Age:", age)
+
+if st.button("Celebrate"):
+    st.balloons()
